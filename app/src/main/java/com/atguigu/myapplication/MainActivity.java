f6e0b59cc70ffb852b0f581d9f91dae52206856b
helloZWQ123 <517940550@qq.com>
2017-01-06 11:15:13 +0800

    第一次上传

---
+++ app/src/main/java/com/atguigu/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.atguigu.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "第一次成功", Toast.LENGTH_SHORT).show();
+    }
+}
